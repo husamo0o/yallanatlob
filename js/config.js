326a092037a114b94574d629d425029fa09efbe1
husamo0o <husam636@gmail.com>
2017-07-08 04:04:51 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://192.168.43.47/MultiRestaurant/mobileapp/api",
+	'DialogDefaultTitle' : "Talabatna",
+	'pushNotificationSenderid' : "61636096837",
+	'facebookAppId' : "YOUR_FACEBOOK_APP_ID",
+	'APIHasKey' : "ad84a28659a1a491f358787170e5ec1c"
+};
